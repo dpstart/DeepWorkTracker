8721abbb9c281c3a51bf58d1a5290035599ff306
dpstart <danielepaliotta96@gmail.com>
2017-02-26 22:28:42 +0100

    Initial Commit

---
+++ DeepWorkTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeepWorkTracker
+//
+//  Created by Daniele Paliotta on 26/02/17.
+//  Copyright © 2017 Daniele Paliotta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
